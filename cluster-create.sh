f65afa90b5d36a3176f929639df0b1767d4a3296
mprestin77 <54962742+mprestin77@users.noreply.github.com>
2026-05-15 15:12:18 -0700

    modified:   cluster-create.sh 	modified:   cluster-template.yaml

--- cluster-create.sh
+++ cluster-create.sh
@@ -1,21 +1,20 @@
-set -x
-COMPUTE_COMPARTMENT_ID=ocid1.compartment.oc1..aaaaaaaap54r7edm7qrq2wmpiumcmhr6tj6iz2eqiq7uc57xpqggml32q4xa \
-NETWORK_COMPARTMENT_ID=ocid1.compartment.oc1..aaaaaaaatbw2dg23vvqyaikcmxws5rhqe5u6cc42jxoixbxdfdulc5cjzraa \
-OCI_SSH_KEY=$(cat ~/.ssh/id_rsa.pub) \
-KUBERNETES_VERSION=v1.34.3 \
-OCI_IMAGE_ID=ocid1.image.oc1.iad.aaaaaaaampi5gpfcc37aijpfii5nxsaf6ndrrcyx3z2eevz4cmrmtlh35ega \
-CONTROL_PLANE_MACHINE_COUNT=1 \
-NAMESPACE=default \
-WORKER_MACHINE_COUNT=2 \
-OCI_NODE_MACHINE_TYPE=VM.Standard.E5.Flex \
-OCI_NODE_MACHINE_TYPE_OCPUS=2 \
-OCI_NODE_MACHINE_TYPE_MEMORY_IN_GBS=32 \
-OCI_MACHINE_POOL_CIDR_BLOCKS=10.0.104.0/22 \
-OCI_MACHINE_POOL_IP_COUNT=32 \
-CLUSTER_POD_CIDR=10.0.104.0/22 \
-VCN_ID=ocid1.vcn.oc1.iad.amaaaaaa22cz7wqayxhffum4bhsu6wywl5vw5fhu3vy4mq7gtw3wbugect3q \
-SUBNET_CONTROL_PLANE_ENDPOINT_ID=ocid1.subnet.oc1.iad.aaaaaaaasl77s5c4kzoekxqb3yc3ucw5qgsoi7a64ms32pqmrr2pzg5hgmja \
-SUBNET_CONTROL_PLANE_ID=ocid1.subnet.oc1.iad.aaaaaaaatmyiu3fuqt2khszw5f7dqyhvduzw4fpko6uriqstcfzeqaprbpma \
-SUBNET_WORKER_ID=ocid1.subnet.oc1.iad.aaaaaaaatmyiu3fuqt2khszw5f7dqyhvduzw4fpko6uriqstcfzeqaprbpma \
+export COMPUTE_COMPARTMENT_ID="<compute-compartment-ocid>"
+export NETWORK_COMPARTMENT_ID="<network-compartment-ocid>"
+export OCI_SSH_KEY=$(cat <path to SSH public key>)
+export KUBERNETES_VERSION=<Kubernetes version> # Example: v1.34.3
+export OCI_IMAGE_ID="<image-ocid>"
+export CONTROL_PLANE_MACHINE_COUNT=<number of control plane nodes>
+export NAMESPACE=default
+export WORKER_MACHINE_COUNT=<number of worker nodes>
+export OCI_NODE_MACHINE_TYPE=VM.Standard.E5.Flex
+export OCI_NODE_MACHINE_TYPE_OCPUS=<number of OCPU per worker node>
+export OCI_NODE_MACHINE_TYPE_MEMORY_IN_GBS=<memory in GB per worker node>
+export OCI_MACHINE_POOL_CIDR_BLOCKS="<machine-pool-cidr>" # Example: 10.0.104.0/22
+export OCI_MACHINE_POOL_IP_COUNT=<number of pod IPs per worker node>  # Max number of pods per worker node
+export VCN_ID="<vcn-ocid>"
+export SUBNET_CONTROL_PLANE_ENDPOINT_ID="<control-plane-endpoint-subnet-ocid>"
+export SUBNET_CONTROL_PLANE_ID="<control-plane-subnet-ocid>"
+export SUBNET_WORKER_ID="<worker-subnet-ocid>"
+
 clusterctl generate cluster test \
 --from cluster-template.yaml > rendered.yaml
